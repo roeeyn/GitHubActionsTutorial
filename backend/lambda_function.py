addabf7ec6d267ffa358a26691f3daa11bf84be9
Rodrigo Medina <rodrigom@lyft.com>
2022-02-25 23:13:35 -0600

    Validating backend workflow works

--- backend/lambda_function.py
+++ backend/lambda_function.py
@@ -6,7 +6,7 @@ def create_message(name):
 
 
 def lambda_handler(event, context):
-    default_name = "la lambda v1"
+    default_name = "la lambda v2"
     name = event.get("queryStringParameters", {}).get("message", default_name)
     message = create_message(name)
     return {"statusCode": 200, "body": json.dumps(message)}
